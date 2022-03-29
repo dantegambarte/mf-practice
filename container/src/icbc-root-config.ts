cabeb533e807805579b44aa9f1141af5349846c7
dantegambarte <da.nt3@hotmail.com>
2022-03-29 18:45:47 -0300

    Init - Project mf

---
+++ container/src/icbc-root-config.ts
@@ -0,0 +1,17 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@icbc/mf-login",
+  app: () => System.import("@icbc/mf-login"),
+  activeWhen: ["/login"]
+});
+
+registerApplication({
+  name: "@icbc/mf-main",
+  app: () => System.import("@icbc/mf-main"),
+  activeWhen: ["/main"]
+});
+
+start({
+  urlRerouteOnly: true,
+});
---
+++ login/mf-login/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
